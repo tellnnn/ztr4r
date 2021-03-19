562c82b9f905c7cd3317493f9f8eeebb2ec23eb4
tellnnn <tellurium3262@gmail.com>
2021-03-19 13:38:59 +0900

    move user data directory creation into ztr4r-package.R

--- R/ztr4r-package.R
+++ R/ztr4r-package.R
@@ -9,5 +9,13 @@
 #' @keywords internal
 "_PACKAGE"
 
-# The internal environment where user information is stored.
+# The internal environment where user information is stored for a session.
 .env_ztr4r <- new.env(parent = emptyenv())
+
+# The user data directory where user information is stored.
+# check if there is the user data directory
+dir_path <- rappdirs::user_data_dir(appname = "ztr4r", appauthor = "R")
+# if not, create it
+if(!dir.exists(dir_path)) {
+  dir.create(path = dir_path)
+}
