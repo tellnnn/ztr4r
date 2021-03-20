f2e8779811d063c9ce7e7d7134fd913f363646bf
tellnnn <tellurium3262@gmail.com>
2021-03-20 18:01:12 +0900

    rename dir_path with user_dir_path

--- R/ztr4r-package.R
+++ R/ztr4r-package.R
@@ -11,8 +11,8 @@ rlang::`%||%`
 
 # The user data directory where user information is stored.
 # check if there is the user data directory
-dir_path <- rappdirs::user_data_dir(appname = "ztr4r", appauthor = "R")
+user_dir_path <- rappdirs::user_data_dir(appname = "ztr4r", appauthor = "R")
 # if not, create it
-if(!dir.exists(dir_path)) {
-  dir.create(path = dir_path)
+if(!dir.exists(user_dir_path)) {
+  dir.create(path = user_dir_path)
 }
