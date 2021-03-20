697a638d2ed4f6cc8f7b3c6422a576f0b092fcc9
tellnnn <tellurium3262@gmail.com>
2021-03-20 17:32:12 +0900

    add import/export rlang::%||%

--- R/ztr4r-package.R
+++ R/ztr4r-package.R
@@ -1,16 +1,13 @@
 #' ztr4r: Zotero Web API wrapper and tool set for R
 #'
 #' ztr4r provides users Zotero Web API wrapper in R and a tool set for manipulating the contents in a Zotero library.
-#'
-#' @examples
-#' \dontrun{
-#' library(ztr4r)
-#' }
 #' @keywords internal
 "_PACKAGE"
 
-#' @import rlang
-NULL
+
+#' @importFrom rlang %||%
+#' @export
+rlang::`%||%`
 
 # The internal environment where user information is stored for a session.
 .env_ztr4r <- new.env(parent = emptyenv())
